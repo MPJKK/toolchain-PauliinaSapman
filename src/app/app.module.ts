9b64188b3ef2ac29bed98e8d8352b0b8cf724b6e
Pauliina Sapman <pauliips@ety-a2123b-29.edu.wks.metropolia.fi>
2018-01-16 12:13:13 +0200

    viikko 1 tehtävä 1 a ja b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    MainContentComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
